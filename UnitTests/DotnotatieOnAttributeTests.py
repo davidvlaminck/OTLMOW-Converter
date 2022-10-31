f83161dc172e9f559a7995f427993a70a8803ca5
davidvlaminck <davidvlaminck85@gmail.com>
2022-10-31 10:02:01 +0100

    moved tests from otlmow_model

---
+++ UnitTests/DotnotatieOnAttributeTests.py
@@ -0,0 +1,64 @@
+﻿from unittest import TestCase
+
+from UnitTests.TestClasses.Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
+
+
+class DotnotationOnAttributeTests(TestCase):
+    def test_dotnotation_on_attribute(self):
+        instance = AllCasesTestClass()
+
+        with self.subTest('complex attribute'):
+            self.assertEqual('testComplexType', instance._testComplexType.dotnotation)
+            self.assertEqual('testComplexType.testBooleanField', instance.testComplexType._testBooleanField.dotnotation)
+            self.assertEqual('testComplexType.testComplexType2', instance.testComplexType._testComplexType2.dotnotation)
+            self.assertEqual('testComplexType.testComplexType2.testStringField',
+                             instance.testComplexType.testComplexType2._testStringField.dotnotation)
+            self.assertEqual('testComplexType.testComplexType2.testKwantWrd',
+                             instance.testComplexType.testComplexType2._testKwantWrd.dotnotation)
+            self.assertEqual('testComplexType.testComplexType2.testKwantWrd.waarde',
+                             instance.testComplexType.testComplexType2.testKwantWrd._waarde.dotnotation)
+
+        with self.subTest('complex attribute with cardinality'):
+            self.assertEqual('testComplexTypeMetKard[]', instance._testComplexTypeMetKard.dotnotation)
+            self.assertEqual('testComplexTypeMetKard[].testBooleanField',
+                             instance.testComplexTypeMetKard[0]._testBooleanField.dotnotation)
+            self.assertEqual('testComplexTypeMetKard[].testComplexType2MetKard[]',
+                             instance.testComplexTypeMetKard[0]._testComplexType2MetKard.dotnotation)
+            self.assertEqual('testComplexTypeMetKard[].testComplexType2MetKard[].testStringFieldMetKard[]',
+                             instance.testComplexTypeMetKard[0].testComplexType2MetKard[0]._testStringFieldMetKard.dotnotation)
+
+        with self.subTest('non-complex attributes'):
+            self.assertEqual('testKeuzelijst', instance._testKeuzelijst.dotnotation)
+            self.assertEqual('testStringField', instance._testStringField.dotnotation)
+            self.assertEqual('testBooleanField', instance._testBooleanField.dotnotation)
+            self.assertEqual('testDecimalField', instance._testDecimalField.dotnotation)
+
+        with self.subTest('non-complex attribute with cardinality'):
+            self.assertEqual('testKeuzelijstMetKard[]', instance._testKeuzelijstMetKard.dotnotation)
+            self.assertEqual('testStringFieldMetKard[]', instance._testStringFieldMetKard.dotnotation)
+            self.assertEqual('testDecimalFieldMetKard[]', instance._testDecimalFieldMetKard.dotnotation)
+
+        with self.subTest('dte attribute'):
+            self.assertEqual('testEenvoudigType', instance._testEenvoudigType.dotnotation)
+
+        with self.subTest('kwant waarde attribute'):
+            self.assertEqual('testKwantWrd', instance._testKwantWrd.dotnotation)
+            self.assertEqual('testKwantWrd.waarde', instance.testKwantWrd._waarde.dotnotation)
+            self.assertEqual('testKwantWrd.standaardEenheid', instance.testKwantWrd._standaardEenheid.dotnotation)
+            self.assertEqual('testKwantWrdMetKard[]', instance._testKwantWrdMetKard.dotnotation)
+            self.assertEqual('testKwantWrdMetKard[].waarde', instance.testKwantWrdMetKard[0]._waarde.dotnotation)
+            self.assertEqual('testKwantWrdMetKard[].standaardEenheid',
+                             instance.testKwantWrdMetKard[0]._standaardEenheid.dotnotation)
+
+        with self.subTest('union attribute'):
+            self.assertEqual('testUnionType', instance._testUnionType.dotnotation)
+            self.assertEqual('testUnionType.unionString', instance.testUnionType._unionString.dotnotation)
+            self.assertEqual('testUnionType.unionKwantWrd', instance.testUnionType._unionKwantWrd.dotnotation)
+            self.assertEqual('testUnionType.unionKwantWrd.waarde', instance.testUnionType.unionKwantWrd._waarde.dotnotation)
+
+        with self.subTest('union attribute with cardinality'):
+            self.assertEqual('testUnionTypeMetKard[]', instance._testUnionTypeMetKard.dotnotation)
+            self.assertEqual('testUnionTypeMetKard[].unionString', instance.testUnionTypeMetKard[0]._unionString.dotnotation)
+            self.assertEqual('testUnionTypeMetKard[].unionKwantWrd', instance.testUnionTypeMetKard[0]._unionKwantWrd.dotnotation)
+            self.assertEqual('testUnionTypeMetKard[].unionKwantWrd.waarde',
+                             instance.testUnionTypeMetKard[0].unionKwantWrd._waarde.dotnotation)
---
+++ otlmow_converter/DotnotationHelper.py
@@ -0,0 +1,229 @@
+﻿from typing import Union
+
+
+class DotnotationHelper:
+    separator = '.'
+    cardinality_separator = '|'
+    cardinality_indicator = '[]'
+    waarde_shortcut_applicable = False
+
+    @staticmethod
+    def set_class_vars_to_parameters(cardinality_indicator, separator, waarde_shortcut_applicable):
+        DotnotationHelper.separator = separator
+        DotnotationHelper.cardinality_indicator = cardinality_indicator
+        DotnotationHelper.waarde_shortcut_applicable = waarde_shortcut_applicable
+
+    @staticmethod
+    def set_parameters_to_class_vars(cardinality_indicator, separator, waarde_shortcut_applicable):
+        if separator == '':
+            separator = DotnotationHelper.separator
+        if cardinality_indicator == '':
+            cardinality_indicator = DotnotationHelper.cardinality_indicator
+        if waarde_shortcut_applicable is None:
+            waarde_shortcut_applicable = DotnotationHelper.waarde_shortcut_applicable
+        return cardinality_indicator, separator, waarde_shortcut_applicable
+
+    @staticmethod
+    def get_dotnotation(attribute,
+                        separator: str = '',
+                        cardinality_indicator: str = '',
+                        waarde_shortcut_applicable: Union[bool, None] = None):
+
+        cardinality_indicator, separator, waarde_shortcut_applicable = DotnotationHelper.set_parameters_to_class_vars(
+            cardinality_indicator, separator, waarde_shortcut_applicable)
+
+        if waarde_shortcut_applicable:
+            if attribute.naam == 'waarde' and attribute.owner._parent is not None and attribute.owner._parent.field.waarde_shortcut_applicable:
+                return attribute.owner._parent.dotnotation
+
+        dotnotation = attribute.naam
+        if attribute.kardinaliteit_max != '1':
+            dotnotation += cardinality_indicator
+
+        if attribute.owner._parent is not None:
+            return attribute.owner._parent.dotnotation + separator + dotnotation
+
+        return dotnotation
+
+    @staticmethod
+    def get_attributes_by_dotnotation(instance_or_attribute,
+                                      dotnotation: str,
+                                      separator: str = '',
+                                      cardinality_indicator: str = '',
+                                      waarde_shortcut_applicable: Union[bool, None] = None):
+        """Returns the attribute matching the dotnotation starting from a given class instance name or attribute
+        :param instance_or_attribute: class or attribute to start the dotnotation from
+        :param dotnotation: a string representing a hierarchical structure of attributes
+        :type: str
+        :return: returns the attribute matching the dotnotation starting from a given class instance name or attribute
+        :rtype: OTLAttribuut or a list of OTLAttribuut
+        """
+        cardinality_indicator, separator, waarde_shortcut_applicable = DotnotationHelper.set_parameters_to_class_vars(
+            cardinality_indicator, separator, waarde_shortcut_applicable)
+
+        if waarde_shortcut_applicable and separator not in dotnotation:
+            attribute = getattr(instance_or_attribute, '_' + dotnotation.replace(cardinality_indicator, ''))
+            if attribute.field.waarde_shortcut_applicable:
+                waardenObject = getattr(instance_or_attribute, dotnotation.replace(cardinality_indicator, ''))
+                if cardinality_indicator in dotnotation:
+                    collected_attributes = []
+                    for waardenObject_in_list in waardenObject:
+                        collected_attributes.append(getattr(waardenObject_in_list, '_waarde'))
+                    return collected_attributes
+                else:
+                    return getattr(waardenObject, '_waarde')
+
+        if separator in dotnotation:
+            first = dotnotation.split(separator)[0]
+            rest = dotnotation.split(separator, 1)[1]
+            if cardinality_indicator in first:
+                first = first.replace(cardinality_indicator, '')
+                attribute = getattr(instance_or_attribute, first)
+                collected_attributes = []
+                for attr_in_list in attribute:
+                    collected_attributes.append(DotnotationHelper.get_attributes_by_dotnotation(attr_in_list, rest))
+                return collected_attributes
+            else:
+                attribute = getattr(instance_or_attribute, first)
+                return DotnotationHelper.get_attributes_by_dotnotation(attribute, rest)
+        else:
+            if cardinality_indicator in dotnotation:
+                return getattr(instance_or_attribute, '_' + dotnotation.replace(cardinality_indicator, ''))
+            else:
+                return getattr(instance_or_attribute, '_' + dotnotation)
+
+    @staticmethod
+    def set_attribute_by_dotnotation(instanceOrAttribute, dotnotation, value, convert=True, convert_warnings: bool = True,
+                                     separator: str = '', cardinality_indicator: str = '',
+                                     waarde_shortcut_applicable: Union[bool, None] = None) -> None:
+
+        cardinality_indicator, separator, waarde_shortcut_applicable = DotnotationHelper.set_parameters_to_class_vars(
+            cardinality_indicator, separator, waarde_shortcut_applicable)
+
+        if dotnotation.count(cardinality_indicator) == 0:
+            gets = DotnotationHelper.get_attributes_by_dotnotation(instance_or_attribute=instanceOrAttribute,
+                                                                   dotnotation=dotnotation,
+                                                                   separator=separator,
+                                                                   cardinality_indicator=cardinality_indicator,
+                                                                   waarde_shortcut_applicable=waarde_shortcut_applicable)
+            if convert:
+                gets.set_waarde(DotnotationHelper.convert_waarde_to_correct_type(value, gets, convert_warnings))
+            else:
+                gets.set_waarde(value)
+            return
+
+        # there is a cardinality_indicator in the dotnotation
+
+        if separator not in dotnotation:
+            # set list directly
+            attribute = DotnotationHelper.get_attributes_by_dotnotation(instance_or_attribute=instanceOrAttribute,
+                                                                        dotnotation=dotnotation.replace(cardinality_indicator, ''),
+                                                                        separator=separator,
+                                                                        cardinality_indicator=cardinality_indicator,
+                                                                        waarde_shortcut_applicable=False)
+
+            if not isinstance(attribute, list) and not attribute.field.waarde_shortcut_applicable:
+                if convert:
+                    converted_value = DotnotationHelper.convert_waarde_to_correct_type(value, attribute, convert_warnings)
+                    attribute.set_waarde(converted_value)
+                else:
+                    attribute.set_waarde(value)
+                return
+
+            # waarde shortcut
+            for index, list_item in enumerate(value):
+                if attribute.waarde is None or len(attribute.waarde) <= index:
+                    attribute.add_empty_value()
+                DotnotationHelper.set_attribute_by_dotnotation(instanceOrAttribute=attribute.waarde[index],
+                                                               dotnotation='waarde',
+                                                               value=list_item,
+                                                               convert=convert,
+                                                               separator=separator,
+                                                               cardinality_indicator=cardinality_indicator,
+                                                               waarde_shortcut_applicable=waarde_shortcut_applicable)
+            return
+
+        # cardinality > 1 and separator => search cardinality indicator
+        first = dotnotation.split(separator)[0]
+        rest = dotnotation.split(separator, maxsplit=1)[1]
+
+        if cardinality_indicator not in first:
+            # cardinality indicator not in first part => go one deeper
+            attribute = DotnotationHelper.get_attributes_by_dotnotation(instance_or_attribute=instanceOrAttribute,
+                                                                        dotnotation=first,
+                                                                        separator=separator,
+                                                                        cardinality_indicator=cardinality_indicator,
+                                                                        waarde_shortcut_applicable=waarde_shortcut_applicable)
+            if attribute.waarde is None:
+                attribute.add_empty_value()
+            DotnotationHelper.set_attribute_by_dotnotation(attribute.waarde, rest, value)
+            return
+
+        if cardinality_indicator in first:
+            # shortcut waarde can't be applicable to this attribute because there is still a 2nd part in dotnotation
+            # this must be a union / complex type
+            attribute = DotnotationHelper.get_attributes_by_dotnotation(instance_or_attribute=instanceOrAttribute,
+                                                                        dotnotation=first.replace(cardinality_indicator, ''),
+                                                                        separator=separator,
+                                                                        cardinality_indicator=cardinality_indicator,
+                                                                        waarde_shortcut_applicable=False)
+
+            for index, value_item in enumerate(value):
+                if attribute.waarde is None or len(attribute.waarde) <= index:
+                    attribute.add_empty_value()
+                DotnotationHelper.set_attribute_by_dotnotation(instanceOrAttribute=attribute.waarde[index], dotnotation=rest,
+                                                               value=value_item, convert=convert, separator=separator,
+                                                               cardinality_indicator=cardinality_indicator,
+                                                               waarde_shortcut_applicable=waarde_shortcut_applicable)
+
+    @staticmethod
+    def convert_waarde_to_correct_type(waarde, attribuut, log_warnings):
+        field = attribuut.field
+        if attribuut.kardinaliteit_max != '1' and isinstance(waarde, list):
+            new_list = []
+            for value_item in waarde:
+                new_list.append(field.convert_to_correct_type(value_item, log_warnings=log_warnings))
+            return new_list
+
+        if attribuut.field.waardeObject is not None and attribuut.field.waarde_shortcut_applicable:
+            field = attribuut.waarde._waarde.field
+
+        return field.convert_to_correct_type(waarde, log_warnings=log_warnings)
+
+    def flatten_dict(self, input_dict: dict, seperator: str = '.', prefix='', affix='', new_dict=None):
+        if new_dict is None:
+            new_dict = {}
+        for k, v in input_dict.items():
+            if isinstance(v, dict):
+                self.flatten_dict(input_dict=v, prefix=k, new_dict=new_dict)
+            elif isinstance(v, list):
+                for i in range(0, len(v)):
+                    if isinstance(v[i], dict):
+                        self.flatten_dict(input_dict=v[i], prefix=k, affix='[' + str(i) + ']', new_dict=new_dict)
+                    else:
+                        if prefix != '':
+                            new_dict[prefix + seperator + k + '[' + str(i) + ']'] = v[i]
+                        else:
+                            new_dict[k + '[' + str(i) + ']'] = v[i]
+            else:
+                if prefix != '':
+                    new_dict[prefix + affix + seperator + k] = v
+                else:
+                    new_dict[k] = v
+
+        return new_dict
+
+
+# from collections.abc import MutableMapping
+#
+# def _flatten_dict_gen(d, parent_key, sep):
+#     for k, v in d.items():
+#         new_key = parent_key + sep + k if parent_key else k
+#         if isinstance(v, MutableMapping):
+#             yield from flatten_dict(v, new_key, sep=sep).items()
+#         else:
+#             yield new_key, v
+#
+#
+# def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str = '.'):
+#     return dict(_flatten_dict_gen(d, parent_key, sep))
