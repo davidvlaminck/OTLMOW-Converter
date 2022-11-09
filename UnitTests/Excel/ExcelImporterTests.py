885f501cb1a2a396aec8258d642ab577bbbc76c8
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-09 11:01:22 +0100

    added import tests similar to csv

--- UnitTests/Excel/ExcelImporterTests.py
+++ UnitTests/Excel/ExcelImporterTests.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 from pathlib import Path
+from datetime import date, datetime, time
 
 import pandas
 from otlmow_model.Classes.Onderdeel.Verkeersregelaar import Verkeersregelaar
@@ -13,12 +14,11 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class ExcelImporterTests(unittest.TestCase):
     def test_init_importer_only_load_with_settings(self):
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        settings_file_location = f'{base_dir}/../settings_OTLMOW.json'
-        otl_facility = OtlmowConverter(logfile='', settings_path=settings_file_location)
+        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+        converter = OtlmowConverter(settings_path=settings_file_location)
 
         with self.subTest('load with correct settings'):
-            importer = ExcelImporter(settings=otl_facility.settings)
+            importer = ExcelImporter(settings=converter.settings)
             self.assertIsNotNone(importer)
 
         with self.subTest('load without settings'):
@@ -34,12 +34,11 @@ class ExcelImporterTests(unittest.TestCase):
                 ExcelImporter(settings={"file_formats": [{}]})
 
     def test_load_test_file(self):
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        settings_file_location = f'{base_dir}/../settings_OTLMOW.json'
-        otl_facility = OtlmowConverter(logfile='', settings_path=settings_file_location)
-        importer = ExcelImporter(settings=otl_facility.settings)
-        file_location = os.path.abspath(os.path.join(os.sep, ROOT_DIR, 'test_file_VR.xlsx'))
-        importer.import_file(Path(file_location))
+        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+        converter = OtlmowConverter(settings_path=settings_file_location)
+        importer = ExcelImporter(settings=converter.settings)
+        file_location = Path(__file__).parent / 'test_file_VR.xlsx'
+        importer.import_file(file_location)
         objects = importer.create_objects_from_data()
         self.assertEqual(132, len(objects))
         vrs = list(filter(lambda x: Verkeersregelaar.typeURI in x.typeURI, objects))
@@ -50,10 +49,9 @@ class ExcelImporterTests(unittest.TestCase):
         self.assertEqual('bij externe partij 1', vrs[0].externeReferentie[1].externePartij)
 
     def test_create_objects_from_data(self):
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        settings_file_location = f'{base_dir}/../settings_OTLMOW.json'
-        otl_facility = OtlmowConverter(logfile='', settings_path=settings_file_location)
-        importer = ExcelImporter(settings=otl_facility.settings)
+        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+        converter = OtlmowConverter(settings_path=settings_file_location)
+        importer = ExcelImporter(settings=converter.settings)
 
         df = pandas.DataFrame(columns='typeURI	assetId.identificator	assetId.toegekendDoor	coordinatiewijze[]	datumOprichtingObject	externeReferentie[].externReferentienummer	externeReferentie[].externePartij	isActief	kabelaansluitschema.bestandsnaam	kabelaansluitschema.uri	naam	notitie	programmeertool	regelaartype	technischeDocumentatie.bestandsnaam	technischeDocumentatie.uri	toestand	voltageLampen	vplanDatum	vplanNummer	geometry'.split('\t'),
                               data=[['https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Verkeersregelaar','f184a415-10c3-465a-85d0-5e3495e7c57f-b25kZXJkZWVsI1ZlcmtlZXJzcmVnZWxhYXI','AWV','centraal|pulsen','2020-03-14','externe referentie / 2|externe referentie 1','-|bij externe partij 1','true','5_zon_maan_aarde.pdf','/eminfra/core/api/otl/assets/f184a415-10c3-465a-85d0-5e3495e7c57f-b25kZXJkZWVsI1ZlcmtlZXJzcmVnZWxhYXI/documenten/182512e8-d527-4f36-b823-cf8b4a0fdea8','Gevonden_demo_OTL','test woppa coach','fwispbnxlo','type-1','Volbeat_1 (19).jpg','/eminfra/core/api/otl/assets/f184a415-10c3-465a-85d0-5e3495e7c57f-b25kZXJkZWVsI1ZlcmtlZXJzcmVnZWxhYXI/documenten/6b6c0872-2305-436d-8255-8675e186716c','in-gebruik','230','2020-04-22','V015254v11','POINT Z (155377.8 211520.7 0)']])
@@ -68,4 +66,92 @@ class ExcelImporterTests(unittest.TestCase):
         self.assertTrue(isinstance(objects[0], Verkeersregelaar))
         self.assertEqual('centraal', objects[0].coordinatiewijze[0])
         self.assertEqual('pulsen', objects[0].coordinatiewijze[1])
+
+    def test_load_test_unnested_attributes(self):
+        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+        converter = OtlmowConverter(settings_path=settings_file_location)
+        importer = ExcelImporter(settings=converter.settings)
+        file_location = Path(__file__).parent / 'Testfiles' / 'unnested_attributes.xlsx'
+        objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
+        self.assertEqual(1, len(objects))
+        self.assertEqual(17, len(importer.data['unnested_attributes'].columns))
+
+        instance = objects[0]
+        self.assertEqual('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass', instance.typeURI)
+        self.assertEqual(False, instance.testBooleanField)
+        self.assertEqual(date(2019, 9, 20), instance.testDateField)
+        self.assertEqual(datetime(2001, 12, 15, 22, 22, 15), instance.testDateTimeField)
+        self.assertEqual(79.07, instance.testDecimalField)
+        self.assertListEqual([10.0, 20.0], instance.testDecimalFieldMetKard)
+        self.assertEqual('string1', instance.testEenvoudigType.waarde)
+        self.assertEqual('string1', instance.testEenvoudigTypeMetKard[0].waarde)
+        self.assertEqual('string2', instance.testEenvoudigTypeMetKard[1].waarde)
+        self.assertEqual(-55, instance.testIntegerField)
+        self.assertListEqual([76, 2], instance.testIntegerFieldMetKard)
+        self.assertEqual('waarde-4', instance.testKeuzelijst)
+        self.assertListEqual(['waarde-4', 'waarde-3'], instance.testKeuzelijstMetKard)
+        self.assertEqual(98.21, instance.testKwantWrd.waarde)
+        self.assertEqual(10.0, instance.testKwantWrdMetKard[0].waarde)
+        self.assertEqual(20.0, instance.testKwantWrdMetKard[1].waarde)
+        self.assertEqual('oFfeDLp', instance.testStringField)
+        self.assertEqual('string1', instance.testStringFieldMetKard[0])
+        self.assertEqual('string2', instance.testStringFieldMetKard[1])
+        self.assertEqual(time(11, 5, 26), instance.testTimeField)
+
+    def test_load_test_ested_attributes_1_level(self):
+        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+        converter = OtlmowConverter(settings_path=settings_file_location)
+        importer = ExcelImporter(settings=converter.settings)
+        file_location = Path(__file__).parent / 'Testfiles' / 'nested_attributes_1.xlsx'
+        objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
+        self.assertEqual(1, len(objects))
+        self.assertEqual(15, len(importer.data['nested_attributes_1'].columns))
+
+        instance = objects[0]
+        self.assertEqual('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass', instance.typeURI)
+        self.assertEqual('YKAzZDhhdTXqkD', instance.assetId.identificator)
+        self.assertEqual('DGcQxwCGiBlR', instance.assetId.toegekendDoor)
+        self.assertEqual(True, instance.testComplexType.testBooleanField)
+        self.assertEqual(65.14, instance.testComplexType.testKwantWrd.waarde)
+        self.assertEqual(10.0, instance.testComplexType.testKwantWrdMetKard[0].waarde)
+        self.assertEqual(20.0, instance.testComplexType.testKwantWrdMetKard[1].waarde)
+        self.assertEqual('KmCtMXM', instance.testComplexType.testStringField)
+        self.assertEqual('string1', instance.testComplexType.testStringFieldMetKard[0])
+        self.assertEqual('string2', instance.testComplexType.testStringFieldMetKard[1])
+        self.assertEqual(True, instance.testComplexTypeMetKard[0].testBooleanField)
+        self.assertEqual(False, instance.testComplexTypeMetKard[1].testBooleanField)
+        self.assertEqual(10.0, instance.testComplexTypeMetKard[0].testKwantWrd.waarde)
+        self.assertEqual(20.0, instance.testComplexTypeMetKard[1].testKwantWrd.waarde)
+        self.assertEqual(None, instance.testComplexTypeMetKard[0].testKwantWrdMetKard[0].waarde)
+        self.assertEqual(None, instance.testComplexTypeMetKard[0].testKwantWrdMetKard[0].waarde)
+        self.assertEqual('string1', instance.testComplexTypeMetKard[0].testStringField)
+        self.assertEqual('string2', instance.testComplexTypeMetKard[1].testStringField)
+        self.assertEqual('RWKofW', instance.testUnionType.unionString)
+        self.assertEqual(None, instance.testUnionType.unionKwantWrd.waarde)
+        self.assertEqual(10.0, instance.testUnionTypeMetKard[0].unionKwantWrd.waarde)
+        self.assertEqual(20.0, instance.testUnionTypeMetKard[1].unionKwantWrd.waarde)
+
+    def test_load_test_ested_attributes_2_levels(self):
+        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+        converter = OtlmowConverter(settings_path=settings_file_location)
+        importer = ExcelImporter(settings=converter.settings)
+        file_location = Path(__file__).parent / 'Testfiles' / 'nested_attributes_2.xlsx'
+        objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
+        self.assertEqual(1, len(objects))
+        self.assertEqual(9, len(importer.data['nested_attributes_2'].columns))
+
+        instance = objects[0]
+        self.assertEqual('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass', instance.typeURI)
+        self.assertEqual(76.8, instance.testComplexType.testComplexType2.testKwantWrd.waarde)
+        self.assertEqual('GZBzgRhOrQvfZaN', instance.testComplexType.testComplexType2.testStringField)
+        self.assertEqual(10.0, instance.testComplexType.testComplexType2MetKard[0].testKwantWrd.waarde)
+        self.assertEqual(20.0, instance.testComplexType.testComplexType2MetKard[1].testKwantWrd.waarde)
+        self.assertEqual('string1', instance.testComplexType.testComplexType2MetKard[0].testStringField)
+        self.assertEqual('string2', instance.testComplexType.testComplexType2MetKard[1].testStringField)
+        self.assertEqual(10.0, instance.testComplexTypeMetKard[0].testComplexType2.testKwantWrd.waarde)
+        self.assertEqual(20.0, instance.testComplexTypeMetKard[1].testComplexType2.testKwantWrd.waarde)
+        self.assertEqual('string1', instance.testComplexTypeMetKard[0].testComplexType2.testStringField)
+        self.assertEqual('string2', instance.testComplexTypeMetKard[1].testComplexType2.testStringField)
+        self.assertEqual(None, instance.testComplexTypeMetKard[0].testComplexType2MetKard[0].testKwantWrd.waarde)
+        self.assertEqual(None, instance.testComplexTypeMetKard[0].testComplexType2MetKard[0].testStringField)
 
--- otlmow_converter/FileFormats/ExcelImporter.py
+++ otlmow_converter/FileFormats/ExcelImporter.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 from typing import Dict
@@ -35,22 +36,34 @@ class ExcelImporter:
         except Exception as ex:
             raise ex
 
-        return self.create_objects_from_data()
+        return self.create_objects_from_data(**kwargs)
 
-    def create_objects_from_data(self):
+    def create_objects_from_data(self, **kwargs):
         list_of_objects = []
+        class_directory = None
+        if kwargs is not None:
+            if 'class_directory' in kwargs:
+                class_directory = kwargs['class_directory']
+
         cardinality_indicator = self.settings['dotnotation']['cardinality indicator']
 
         for sheet, dataframe in self.data.items():
             for index, record in dataframe.iterrows():
-                object = AssetFactory().dynamic_create_instance_from_uri(record['typeURI'])
-                list_of_objects.append(object)
+                instance = AssetFactory().dynamic_create_instance_from_uri(record['typeURI'], directory=class_directory)
+                list_of_objects.append(instance)
                 for header, value in record.items():
                     if header in ['typeURI']:
                         continue
 
                     if cardinality_indicator in header:
-                        value = value.split(self.settings['dotnotation']['cardinality separator'])
+                        if header.count(cardinality_indicator) > 1:
+                            logging.warning(f'{header} is a list of lists. This is not allowed in the CSV format')
+                            continue
+
+                        if isinstance(value, str) and self.settings['dotnotation']['cardinality separator'] in value:
+                            value = value.split(self.settings['dotnotation']['cardinality separator'])
+                        elif not isinstance(value, list):
+                            value = [value]
 
                     if header == 'geometry':
                         if value == '':
@@ -58,14 +71,14 @@ class ExcelImporter:
 
                     try:
                         DotnotationHelper.set_attribute_by_dotnotation(
-                            instanceOrAttribute=object, dotnotation=header, value=value, convert_warnings=False,
+                            instanceOrAttribute=instance, dotnotation=header, value=value, convert_warnings=False,
                             separator=self.settings['dotnotation']['separator'],
                             cardinality_indicator=cardinality_indicator,
                             waarde_shortcut_applicable=self.settings['dotnotation']['waarde_shortcut_applicable'])
                     except TypeError as type_error:
                         if 'Expecting a string' in type_error.args[0]:
                             DotnotationHelper.set_attribute_by_dotnotation(
-                                instanceOrAttribute=object, dotnotation=header, value=str(value), convert_warnings=False,
+                                instanceOrAttribute=instance, dotnotation=header, value=str(value), convert_warnings=False,
                                 separator=self.settings['dotnotation']['separator'],
                                 cardinality_indicator=cardinality_indicator,
                                 waarde_shortcut_applicable=self.settings['dotnotation']['waarde_shortcut_applicable'])
