f4c415b6640732c5d13df308d3a2f6cad20b5db7
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-18 15:10:24 +0100

    changed keuzelijsten of testclasses

--- UnitTests/TestClasses/Datatypes/KlAIMToestand.py
+++ UnitTests/TestClasses/Datatypes/KlAIMToestand.py
@@ -58,6 +58,5 @@ class KlAIMToestand(KeuzelijstField):
 
     @classmethod
     def create_dummy_data(cls):
-        return random.choice(list(map(lambda x: x.invulwaarde,
-                                      filter(lambda option: option.status == 'ingebruik', cls.options.values()))))
+        return cls.create_dummy_data_keuzelijst(cls.options)
 
--- UnitTests/TestClasses/Datatypes/KlTestKeuzelijst.py
+++ UnitTests/TestClasses/Datatypes/KlTestKeuzelijst.py
@@ -39,6 +39,5 @@ class KlTestKeuzelijst(KeuzelijstField):
 
     @classmethod
     def create_dummy_data(cls):
-        return random.choice(list(map(lambda x: x.invulwaarde,
-                                      filter(lambda option: option.status == 'ingebruik', cls.options.values()))))
+        return cls.create_dummy_data_keuzelijst(cls.options)
 
