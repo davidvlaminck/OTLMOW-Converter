5d9cfdb3788cd6adf8daed72450a2bc0700b17ba
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2022-12-21 13:31:19 +0100

    changed RDF gen script

--- SampleCode/csv_to_ttl.py
+++ SampleCode/csv_to_ttl.py
@@ -1,13 +1,20 @@
 from datetime import datetime
 from pathlib import Path
 
+from UnitTests.TestClasses.Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
 from otlmow_converter.OtlmowConverter import OtlmowConverter
 
 if __name__ == '__main__':
     otlmow_converter = OtlmowConverter()
 
-    assets = otlmow_converter.create_assets_from_file(Path('/home/davidlinux/Documents/AWV/cameras_open_data_2.json'))
+    instance = AllCasesTestClass()
+    instance.assetId.identificator = '0000'
+    instance.testBooleanField = False
+    instance.testDecimalField = 79.07
+    instance.testIntegerField = -55
+    assets = [instance]
 
-    # export
-    file_path = Path(f'Output/{datetime.now().strftime("%Y%m%d%H%M%S")}_export.jsonld')
+# export
+    file_path = Path(f'Output/{datetime.now().strftime("%Y%m%d%H%M%S")}_testclass.ttl')
+    file_path = Path(f'Output/testclass.ttl')
     otlmow_converter.create_file_from_assets(filepath=file_path, list_of_objects=assets)
