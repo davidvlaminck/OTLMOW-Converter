0f1501e9b3844da31767ddd929cf60a89488e455
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-03-06 12:15:44 +0100

    benchmark action

---
+++ benchmark/perform_benchmark.py
@@ -0,0 +1,33 @@
+import timeit
+from pathlib import Path
+from statistics import mean, stdev
+
+from prettytable import prettytable
+
+from otlmow_converter.OtlmowConverter import OtlmowConverter
+
+
+def load_assets():
+    converter = OtlmowConverter()
+    converter.create_assets_from_file(Path('files/all_classes.csv'))
+
+
+def load_assets2():
+    converter = OtlmowConverter()
+    converter.create_assets_from_file(Path('files/ten_random_classes.csv'))
+
+
+if __name__ == '__main__':
+    result = timeit.repeat(load_assets, repeat=6, number=1)[1:]
+    stdev1 = stdev(result)
+    result2 = timeit.repeat(load_assets2, repeat=3, number=1)[1:]
+    stdev2 = stdev(result2)
+
+    with open("benchmark_results.txt", "w") as file:
+        file.writelines(['Benchmarking results\n'])
+
+        tb = prettytable.PrettyTable()
+        tb.field_names = ["Format", "Read all classes", "Read 10 random classes"]
+        tb.add_row(['CSV', f'{round(mean(result), 3)} +/- {round(stdev1, 3)}',
+                    f'{round(mean(result2), 3)} +/- {round(stdev2, 3)}'])
+        file.writelines(str(tb))
