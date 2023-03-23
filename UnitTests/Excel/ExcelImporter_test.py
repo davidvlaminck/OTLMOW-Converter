f225da374c363b2ad8514b85057e7c266972f020
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2023-03-23 12:00:39 +0100

    refactor tests

--- UnitTests/Excel/ExcelImporter_test.py
+++ UnitTests/Excel/ExcelImporter_test.py
@@ -1,7 +1,8 @@
 import os
-import unittest
 from datetime import date, datetime, time
 from pathlib import Path
+
+import pytest
 
 from otlmow_converter.FileFormats.ExcelImporter import ExcelImporter
 from otlmow_converter.OtlmowConverter import OtlmowConverter
@@ -9,109 +10,111 @@ from otlmow_converter.OtlmowConverter import OtlmowConverter
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-class ExcelImporterTests(unittest.TestCase):
-    def test_init_importer_only_load_with_settings(self):
-        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
-        converter = OtlmowConverter(settings_path=settings_file_location)
+def test_init_importer_only_load_with_settings(subtests):
+    settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+    converter = OtlmowConverter(settings_path=settings_file_location)
 
-        with self.subTest('load with correct settings'):
-            importer = ExcelImporter(settings=converter.settings)
-            self.assertIsNotNone(importer)
-
-        with self.subTest('load without settings'):
-            with self.assertRaises(ValueError):
-                ExcelImporter(settings=None)
-
-        with self.subTest('load with incorrect settings (no file_formats)'):
-            with self.assertRaises(ValueError):
-                ExcelImporter(settings={"auth_options": [{}]})
-
-        with self.subTest('load with incorrect settings (file_formats but no xls)'):
-            with self.assertRaises(ValueError):
-                ExcelImporter(settings={"file_formats": [{}]})
-
-    def test_load_test_unnested_attributes(self):
-        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
-        converter = OtlmowConverter(settings_path=settings_file_location)
+    with subtests.test(msg='load with correct settings'):
         importer = ExcelImporter(settings=converter.settings)
-        file_location = Path(__file__).parent / 'Testfiles' / 'unnested_attributes.xlsx'
-        objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
-        self.assertEqual(1, len(objects))
+        assert importer is not None
 
-        instance = objects[0]
-        self.assertEqual('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass', instance.typeURI)
-        self.assertEqual(False, instance.testBooleanField)
-        self.assertEqual(date(2019, 9, 20), instance.testDateField)
-        self.assertEqual(datetime(2001, 12, 15, 22, 22, 15), instance.testDateTimeField)
-        self.assertEqual(79.07, instance.testDecimalField)
-        self.assertListEqual([10.0, 20.0], instance.testDecimalFieldMetKard)
-        self.assertEqual('string1', instance.testEenvoudigType.waarde)
-        self.assertEqual('string1', instance.testEenvoudigTypeMetKard[0].waarde)
-        self.assertEqual('string2', instance.testEenvoudigTypeMetKard[1].waarde)
-        self.assertEqual(-55, instance.testIntegerField)
-        self.assertListEqual([76, 2], instance.testIntegerFieldMetKard)
-        self.assertEqual('waarde-4', instance.testKeuzelijst)
-        self.assertListEqual(['waarde-4', 'waarde-3'], instance.testKeuzelijstMetKard)
-        self.assertEqual(98.21, instance.testKwantWrd.waarde)
-        self.assertEqual(10.0, instance.testKwantWrdMetKard[0].waarde)
-        self.assertEqual(20.0, instance.testKwantWrdMetKard[1].waarde)
-        self.assertEqual('oFfeDLp', instance.testStringField)
-        self.assertEqual('string1', instance.testStringFieldMetKard[0])
-        self.assertEqual('string2', instance.testStringFieldMetKard[1])
-        self.assertEqual(time(11, 5, 26), instance.testTimeField)
+    with subtests.test(msg='load without settings'):
+        with pytest.raises(ValueError):
+            ExcelImporter(settings=None)
 
-    def test_load_test_ested_attributes_1_level(self):
-        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
-        converter = OtlmowConverter(settings_path=settings_file_location)
-        importer = ExcelImporter(settings=converter.settings)
-        file_location = Path(__file__).parent / 'Testfiles' / 'nested_attributes_1.xlsx'
-        objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
-        self.assertEqual(1, len(objects))
+    with subtests.test(msg='load with incorrect settings (no file_formats)'):
+        with pytest.raises(ValueError):
+            ExcelImporter(settings={"auth_options": [{}]})
 
-        instance = objects[0]
-        self.assertEqual('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass', instance.typeURI)
-        self.assertEqual('YKAzZDhhdTXqkD', instance.assetId.identificator)
-        self.assertEqual('DGcQxwCGiBlR', instance.assetId.toegekendDoor)
-        self.assertEqual(True, instance.testComplexType.testBooleanField)
-        self.assertEqual(65.14, instance.testComplexType.testKwantWrd.waarde)
-        self.assertEqual(10.0, instance.testComplexType.testKwantWrdMetKard[0].waarde)
-        self.assertEqual(20.0, instance.testComplexType.testKwantWrdMetKard[1].waarde)
-        self.assertEqual('KmCtMXM', instance.testComplexType.testStringField)
-        self.assertEqual('string1', instance.testComplexType.testStringFieldMetKard[0])
-        self.assertEqual('string2', instance.testComplexType.testStringFieldMetKard[1])
-        self.assertEqual(True, instance.testComplexTypeMetKard[0].testBooleanField)
-        self.assertEqual(False, instance.testComplexTypeMetKard[1].testBooleanField)
-        self.assertEqual(10.0, instance.testComplexTypeMetKard[0].testKwantWrd.waarde)
-        self.assertEqual(20.0, instance.testComplexTypeMetKard[1].testKwantWrd.waarde)
-        self.assertEqual(None, instance.testComplexTypeMetKard[0].testKwantWrdMetKard[0].waarde)
-        self.assertEqual(None, instance.testComplexTypeMetKard[0].testKwantWrdMetKard[0].waarde)
-        self.assertEqual('string1', instance.testComplexTypeMetKard[0].testStringField)
-        self.assertEqual('string2', instance.testComplexTypeMetKard[1].testStringField)
-        self.assertEqual('RWKofW', instance.testUnionType.unionString)
-        self.assertEqual(None, instance.testUnionType.unionKwantWrd.waarde)
-        self.assertEqual(10.0, instance.testUnionTypeMetKard[0].unionKwantWrd.waarde)
-        self.assertEqual(20.0, instance.testUnionTypeMetKard[1].unionKwantWrd.waarde)
+    with subtests.test(msg='load with incorrect settings (file_formats but no xls)'):
+        with pytest.raises(ValueError):
+            ExcelImporter(settings={"file_formats": [{}]})
 
-    def test_load_test_ested_attributes_2_levels(self):
-        settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
-        converter = OtlmowConverter(settings_path=settings_file_location)
-        importer = ExcelImporter(settings=converter.settings)
-        file_location = Path(__file__).parent / 'Testfiles' / 'nested_attributes_2.xlsx'
-        objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
-        self.assertEqual(1, len(objects))
 
-        instance = objects[0]
-        self.assertEqual('https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass', instance.typeURI)
-        self.assertEqual(76.8, instance.testComplexType.testComplexType2.testKwantWrd.waarde)
-        self.assertEqual('GZBzgRhOrQvfZaN', instance.testComplexType.testComplexType2.testStringField)
-        self.assertEqual(10.0, instance.testComplexType.testComplexType2MetKard[0].testKwantWrd.waarde)
-        self.assertEqual(20.0, instance.testComplexType.testComplexType2MetKard[1].testKwantWrd.waarde)
-        self.assertEqual('string1', instance.testComplexType.testComplexType2MetKard[0].testStringField)
-        self.assertEqual('string2', instance.testComplexType.testComplexType2MetKard[1].testStringField)
-        self.assertEqual(10.0, instance.testComplexTypeMetKard[0].testComplexType2.testKwantWrd.waarde)
-        self.assertEqual(20.0, instance.testComplexTypeMetKard[1].testComplexType2.testKwantWrd.waarde)
-        self.assertEqual('string1', instance.testComplexTypeMetKard[0].testComplexType2.testStringField)
-        self.assertEqual('string2', instance.testComplexTypeMetKard[1].testComplexType2.testStringField)
-        self.assertEqual(None, instance.testComplexTypeMetKard[0].testComplexType2MetKard[0].testKwantWrd.waarde)
-        self.assertEqual(None, instance.testComplexTypeMetKard[0].testComplexType2MetKard[0].testStringField)
+def test_load_test_unnested_attributes():
+    settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+    converter = OtlmowConverter(settings_path=settings_file_location)
+    importer = ExcelImporter(settings=converter.settings)
+    file_location = Path(__file__).parent / 'Testfiles' / 'unnested_attributes.xlsx'
+    objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
+    assert len(objects) == 1
+
+    instance = objects[0]
+    assert instance.typeURI == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass'
+    assert not instance.testBooleanField
+    assert instance.testDateField == date(2019, 9, 20)
+    assert instance.testDateTimeField == datetime(2001, 12, 15, 22, 22, 15)
+    assert instance.testDecimalField == 79.07
+    assert instance.testDecimalFieldMetKard == [10.0, 20.0]
+    assert instance.testEenvoudigType.waarde == 'string1'
+    assert instance.testEenvoudigTypeMetKard[0].waarde == 'string1'
+    assert instance.testEenvoudigTypeMetKard[1].waarde == 'string2'
+    assert instance.testIntegerField == -55
+    assert instance.testIntegerFieldMetKard == [76, 2]
+    assert instance.testKeuzelijst == 'waarde-4'
+    assert instance.testKeuzelijstMetKard == ['waarde-4', 'waarde-3']
+    assert instance.testKwantWrd.waarde == 98.21
+    assert instance.testKwantWrdMetKard[0].waarde == 10.0
+    assert instance.testKwantWrdMetKard[1].waarde == 20.0
+    assert instance.testStringField == 'oFfeDLp'
+    assert instance.testStringFieldMetKard[0] == 'string1'
+    assert instance.testStringFieldMetKard[1] == 'string2'
+    assert instance.testTimeField == time(11, 5, 26)
+
+
+def test_load_test_nested_attributes_1_level():
+    settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+    converter = OtlmowConverter(settings_path=settings_file_location)
+    importer = ExcelImporter(settings=converter.settings)
+    file_location = Path(__file__).parent / 'Testfiles' / 'nested_attributes_1.xlsx'
+    objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
+    assert len(objects) == 1
+
+    instance = objects[0]
+    assert instance.typeURI == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass'
+    assert instance.assetId.identificator == 'YKAzZDhhdTXqkD'
+    assert instance.assetId.toegekendDoor == 'DGcQxwCGiBlR'
+    assert instance.testComplexType.testBooleanField
+    assert instance.testComplexType.testKwantWrd.waarde == 65.14
+    assert instance.testComplexType.testKwantWrdMetKard[0].waarde == 10.0
+    assert instance.testComplexType.testKwantWrdMetKard[1].waarde == 20.0
+    assert instance.testComplexType.testStringField == 'KmCtMXM'
+    assert instance.testComplexType.testStringFieldMetKard[0] == 'string1'
+    assert instance.testComplexType.testStringFieldMetKard[1] == 'string2'
+    assert instance.testComplexTypeMetKard[0].testBooleanField
+    assert not instance.testComplexTypeMetKard[1].testBooleanField
+    assert instance.testComplexTypeMetKard[0].testKwantWrd.waarde == 10.0
+    assert instance.testComplexTypeMetKard[1].testKwantWrd.waarde == 20.0
+    assert instance.testComplexTypeMetKard[0].testKwantWrdMetKard[0].waarde is None
+    assert instance.testComplexTypeMetKard[0].testKwantWrdMetKard[0].waarde is None
+    assert instance.testComplexTypeMetKard[0].testStringField == 'string1'
+    assert instance.testComplexTypeMetKard[1].testStringField == 'string2'
+    assert instance.testUnionType.unionString == 'RWKofW'
+    assert instance.testUnionType.unionKwantWrd.waarde is None
+    assert instance.testUnionTypeMetKard[0].unionKwantWrd.waarde == 10.0
+    assert instance.testUnionTypeMetKard[1].unionKwantWrd.waarde == 20.0
+
+
+def test_load_test_ested_attributes_2_levels():
+    settings_file_location = Path(__file__).parent.parent / 'settings_OTLMOW.json'
+    converter = OtlmowConverter(settings_path=settings_file_location)
+    importer = ExcelImporter(settings=converter.settings)
+    file_location = Path(__file__).parent / 'Testfiles' / 'nested_attributes_2.xlsx'
+    objects = importer.import_file(filepath=file_location, class_directory='UnitTests.TestClasses.Classes')
+    assert len(objects) == 1
+
+    instance = objects[0]
+    assert instance.typeURI == 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass'
+    assert instance.testComplexType.testComplexType2.testKwantWrd.waarde == 76.8
+    assert instance.testComplexType.testComplexType2.testStringField == 'GZBzgRhOrQvfZaN'
+    assert instance.testComplexType.testComplexType2MetKard[0].testKwantWrd.waarde == 10.0
+    assert instance.testComplexType.testComplexType2MetKard[1].testKwantWrd.waarde == 20.0
+    assert instance.testComplexType.testComplexType2MetKard[0].testStringField == 'string1'
+    assert instance.testComplexType.testComplexType2MetKard[1].testStringField == 'string2'
+    assert instance.testComplexTypeMetKard[0].testComplexType2.testKwantWrd.waarde == 10.0
+    assert instance.testComplexTypeMetKard[1].testComplexType2.testKwantWrd.waarde == 20.0
+    assert instance.testComplexTypeMetKard[0].testComplexType2.testStringField == 'string1'
+    assert instance.testComplexTypeMetKard[1].testComplexType2.testStringField == 'string2'
+    assert instance.testComplexTypeMetKard[0].testComplexType2MetKard[0].testKwantWrd.waarde is None
+    assert instance.testComplexTypeMetKard[0].testComplexType2MetKard[0].testStringField is None
 
