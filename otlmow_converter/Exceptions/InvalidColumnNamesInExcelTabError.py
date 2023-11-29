3c05da24ef43a4b571f79726e3c664e840ee9a5f
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-11-29 11:37:29 +0100

    type hint fix

--- otlmow_converter/Exceptions/InvalidColumnNamesInExcelTabError.py
+++ otlmow_converter/Exceptions/InvalidColumnNamesInExcelTabError.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+from typing import List
 
 from otlmow_converter.Exceptions.FailedToImportFileError import FailedToImportFileError
 
 
 class InvalidColumnNamesInExcelTabError(FailedToImportFileError):
-    def __init__(self, message, file_path: Path, tab: str = None, bad_columns: list[str] = None):
+    def __init__(self, message, file_path: Path, tab: str = None, bad_columns: List[str] = None):
         super().__init__(message, file_path)
         self.file_path = file_path
         self.tab = tab
