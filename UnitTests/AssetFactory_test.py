afbc4b29f751352ced2fb7aa8c5672abfe3c4416
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2024-01-25 11:12:21 +0100

    refactored unittest to use testclasses to avoid deprecation warning updated requirements to use model 2.10.0.0 and changed import in AssetFactory

--- UnitTests/AssetFactory_test.py
+++ UnitTests/AssetFactory_test.py
@@ -1,24 +1,28 @@
-﻿from otlmow_model.OtlmowModel.Classes.Onderdeel.Exoten import Exoten
-from otlmow_model.OtlmowModel.Classes.Onderdeel.InvasieveExoten import InvasieveExoten
+﻿from pathlib import Path
 
+from UnitTests.TestModel.OtlmowModel.Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
+from UnitTests.TestModel.OtlmowModel.Classes.Onderdeel.AnotherTestClass import AnotherTestClass
 from otlmow_converter.AssetFactory import AssetFactory
 
 
 def test_create_otl_object_using_other_otl_object_as_template_using_attribute_list():
-    exoten = Exoten()
-    exoten.heeftObstakels = True
-    exoten.breedte.waarde = 2.0
+    another_test_class = AnotherTestClass()
+    another_test_class.notitie = 'notitie'
+    another_test_class.datumOprichtingObject = '2020-01-01'
 
-    attribute_list = [a.naam for a in InvasieveExoten()]
-    result = AssetFactory.create_otl_object_using_other_otl_object_as_template(
-        orig_otl_object=exoten, typeURI=InvasieveExoten.typeURI, fields_to_copy=attribute_list)
+    attribute_list = {a.naam for a in another_test_class} # TODO refactor to use sets
+    attribute_list.remove('deprecatedString')
 
-    input_dict = exoten.create_dict_from_asset()
+    result_asset = AssetFactory.create_otl_object_using_other_otl_object_as_template(
+        orig_otl_object=another_test_class, typeURI=AllCasesTestClass.typeURI, fields_to_copy=attribute_list,
+        model_directory=Path(__file__).parent / 'TestModel')
+
+    input_dict = another_test_class.create_dict_from_asset()
     input_type_uri = input_dict.pop('typeURI')
 
-    result_dict = result.create_dict_from_asset()
+    result_dict = result_asset.create_dict_from_asset()
     result_type_uri = result_dict.pop('typeURI')
 
-    assert result_type_uri == InvasieveExoten.typeURI
-    assert input_type_uri == Exoten.typeURI
+    assert result_type_uri == AllCasesTestClass.typeURI
+    assert input_type_uri == AnotherTestClass.typeURI
     assert result_dict == input_dict
