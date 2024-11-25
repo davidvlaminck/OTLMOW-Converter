f41ba32e7f60d9855d0cbad4af5deabeecb37e64
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-11-25 12:01:26 +0100

    changes to test model and requirements

--- UnitTests/TestModel/OtlmowModel/BaseClasses/RelationInteractor.py
+++ UnitTests/TestModel/OtlmowModel/BaseClasses/RelationInteractor.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod
+from pathlib import Path
+
+from otlmow_model.OtlmowModel.Helpers.generated_lists import get_hardcoded_class_dict, get_concrete_subclasses_from_class_dict
 
 
 class RelationInteractor:
@@ -17,3 +20,24 @@ class RelationInteractor:
         super().__init__()
 
         self._valid_relations = {}
+
+    def _get_all_concrete_relations(self, model_directory: Path = None):
+        class_dict = get_hardcoded_class_dict(model_directory)
+        for relation in self._valid_relations:
+            for target in self._valid_relations[relation]:
+                for direction, deprecated in self._valid_relations[relation][target].items():
+                    if class_dict[target]['abstract']:
+                        for subclass in get_concrete_subclasses_from_class_dict(target, model_directory=model_directory):
+                            if direction == 'o':
+                                yield self.typeURI, relation, subclass, '', deprecated
+                            elif direction == 'i':
+                                yield subclass, relation, self.typeURI, '', deprecated
+                            elif direction == 'u':
+                                yield self.typeURI, relation, subclass, 'Unspecified', deprecated
+                    else:
+                        if direction == 'o':
+                            yield self.typeURI, relation, target, '', deprecated
+                        elif direction == 'i':
+                            yield target, relation, self.typeURI, '', deprecated
+                        elif direction == 'u':
+                            yield self.typeURI, relation, target, 'Unspecified', deprecated
--- UnitTests/TestModel/OtlmowModel/Helpers/generated_lists.py
+++ UnitTests/TestModel/OtlmowModel/Helpers/generated_lists.py
@@ -1,4 +1,5 @@
 import json
+from collections.abc import Iterable
 from pathlib import Path
 
 global_relation_dict: dict = {}
@@ -35,3 +36,10 @@ def get_hardcoded_class_dict(model_directory: Path = None) -> dict:
     global_class_dict = generated_info_dict['classes']
 
     return global_class_dict
+
+def get_concrete_subclasses_from_class_dict(base_uri: str, model_directory: Path = None) -> Iterable[str]:
+    class_dict = get_hardcoded_class_dict(model_directory)
+    for subclass in class_dict[base_uri]['direct_subclasses']:
+        if not class_dict[subclass]['abstract']:
+            yield subclass
+        yield from get_concrete_subclasses_from_class_dict(subclass, model_directory)
