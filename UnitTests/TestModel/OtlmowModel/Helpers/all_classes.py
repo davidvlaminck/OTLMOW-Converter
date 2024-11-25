bc15359dd77037969fbabdda47167ce2f528a18d
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-11-25 12:11:42 +0100

    changes to test model

---
+++ UnitTests/TestModel/OtlmowModel/Helpers/all_classes.py
@@ -0,0 +1,5 @@
+from ..Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
+from ..Classes.Onderdeel.AnotherTestClass import AnotherTestClass
+from ..Classes.Onderdeel.Bevestiging import Bevestiging
+from ..Classes.Onderdeel.DeprecatedTestClass import DeprecatedTestClass
+from ..Classes.Onderdeel.Voedt import Voedt
