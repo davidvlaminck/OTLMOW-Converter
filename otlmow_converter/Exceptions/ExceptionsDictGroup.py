69fd2f44bf33cd72d9124d7949bc9f906bd954cc
christiaan <christiaan.vanbergen.btf@gmail.com>
2024-11-27 15:14:34 +0100

    Add tab/sheet name information with any error happening in ExcelImporter.py

---
+++ otlmow_converter/Exceptions/ExceptionsDictGroup.py
@@ -0,0 +1,31 @@
+from typing import Sequence, Optional
+
+
+class ExceptionsDictGroup(Exception):
+    """
+    Provides the same functions a ExceptionGroup.py but instead of a list of Exceptions it returns
+    a dictionary of structur:
+     {
+        "ex": Exception
+        "tab": str
+    }
+    """
+    def __init__(self, message, exceptions: Sequence[BaseException] = None, cause_tabs: Optional[Sequence[str]]= None):
+        super().__init__(message)
+        if exceptions is None:
+            exceptions = []
+            if cause_tabs:
+                self.exceptions = [{"ex": exceptions[i], "tab": cause_tabs[i]} for i in
+                                   range(len(exceptions))]
+            else:
+                self.exceptions = [{"ex": exceptions[i], "tab": None} for i in
+                                   range(len(exceptions))]
+
+    def add_exception(self, error: BaseException, cause_tab: str = None):
+        self.exceptions.append({"ex": error, "tab": cause_tab})
+
+
+    def __str__(self):
+        return (f'ExceptionGroup with {len(self.exceptions)} error(s): ' +
+                ('\n'.join([str(error) for error in self.exceptions])))
+
