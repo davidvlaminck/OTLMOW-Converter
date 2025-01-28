0dec5b67deb39b143dcef64d55f741d897cc989b
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-01-28 21:32:29 +0100

    changed signature

--- otlmow_converter/AbstractExporter.py
+++ otlmow_converter/AbstractExporter.py
@@ -1,13 +1,14 @@
 import abc
 from pathlib import Path
 from typing import Iterable
-
+from universalasync import async_to_sync_wraps
 from otlmow_model.OtlmowModel.BaseClasses.OTLObject import OTLObject
 
 
 class AbstractExporter(abc.ABC):
     @classmethod
     @abc.abstractmethod
+    @async_to_sync_wraps
     def from_objects(cls, sequence_of_objects: Iterable[OTLObject], filepath: Path, **kwargs) -> None:
         pass
 
--- otlmow_converter/AbstractImporter.py
+++ otlmow_converter/AbstractImporter.py
@@ -1,12 +1,13 @@
 import abc
 from pathlib import Path
 from typing import Iterable
-
+from universalasync import async_to_sync_wraps
 from otlmow_model.OtlmowModel.BaseClasses.OTLObject import OTLObject
 
 
 class AbstractImporter(abc.ABC):
     @classmethod
     @abc.abstractmethod
-    def to_objects(cls, filepath: Path, **kwargs) -> Iterable[OTLObject]:
+    @async_to_sync_wraps
+    async def to_objects(cls, filepath: Path, **kwargs) -> Iterable[OTLObject]:
         pass
