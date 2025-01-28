c10cedac58bae3167c2d387feb6d1686a8bda098
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-01-28 20:54:35 +0100

    pandas converter

--- otlmow_converter/FileFormats/PandasConverter.py
+++ otlmow_converter/FileFormats/PandasConverter.py
@@ -1,28 +1,31 @@
 from pathlib import Path
 from typing import Iterable
-
+from universalasync import async_to_sync_wraps
 from numpy import nan
 from otlmow_model.OtlmowModel.BaseClasses.OTLObject import OTLObject
 from pandas import DataFrame
-
 from otlmow_converter.FileFormats.DotnotationTableConverter import DotnotationTableConverter
 
 
 class PandasConverter:
     @classmethod
-    def convert_objects_to_single_dataframe(cls, list_of_objects: Iterable[OTLObject], **kwargs) -> DataFrame:
-        single_table = DotnotationTableConverter.get_single_table_from_data(list_of_objects=list_of_objects, **kwargs)
+    @async_to_sync_wraps
+    async def convert_objects_to_single_dataframe(cls, list_of_objects: Iterable[OTLObject], **kwargs) -> DataFrame:
+        single_table = await DotnotationTableConverter.get_single_table_from_data(
+            list_of_objects=list_of_objects, **kwargs)
         return DataFrame(data=single_table[1:])
 
     @classmethod
-    def convert_objects_to_multiple_dataframes(cls, sequence_of_objects: Iterable[OTLObject], **kwargs
+    @async_to_sync_wraps
+    async def convert_objects_to_multiple_dataframes(cls, sequence_of_objects: Iterable[OTLObject], **kwargs
                                                ) -> dict[str, DataFrame]:
-        dict_tables = DotnotationTableConverter.get_tables_per_type_from_data(sequence_of_objects=sequence_of_objects,
-                                                                              **kwargs)
+        dict_tables = await DotnotationTableConverter.get_tables_per_type_from_data(
+            sequence_of_objects=sequence_of_objects, **kwargs)
         return {key: DataFrame(data=value[1:]) for key, value in dict_tables.items()}
 
     @classmethod
-    def convert_dataframe_to_objects(cls, dataframe: DataFrame, model_directory: Path = None, **kwargs
+    @async_to_sync_wraps
+    async def convert_dataframe_to_objects(cls, dataframe: DataFrame, model_directory: Path = None, **kwargs
                                      ) -> Iterable[OTLObject]:
         dataframe = dataframe.replace({nan: None})
         headers = list(dataframe)
@@ -30,6 +33,6 @@ class PandasConverter:
         dict_list = [d]
         dict_list.extend(dataframe.to_dict('records'))
 
-        return DotnotationTableConverter.get_data_from_table(table_data=dict_list, model_directory=model_directory,
-                                                             **kwargs)
+        return await DotnotationTableConverter.get_data_from_table(
+            table_data=dict_list,   model_directory=model_directory, **kwargs)
     
